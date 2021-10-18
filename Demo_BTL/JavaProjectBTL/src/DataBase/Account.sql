29e19ef3190985f1f3ba9bfbab43065a7e303bbb
202Sasori <65811291+202Sasori@users.noreply.github.com>
2021-10-18 10:28:20 +0700

    Update Account.sql

--- Demo_BTL/JavaProjectBTL/src/DataBase/Account.sql
+++ Demo_BTL/JavaProjectBTL/src/DataBase/Account.sql
@@ -1,10 +1,13 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  FPT
- * Created: Oct 13, 2021
- */
+CREATE DATABASE Account
+USE Account
 
+CREATE TABLE Account(
+	UserName VARCHAR(50),
+	Pass CHAR(20),
+	Confirmpass CHAR(20),
+	SDT CHAR(11)
+)
+INSERT INTO Account(UserName,Pass,Confirmpass,SDT)
+VALUES ('dinhkhachoat','20200184','20200184','0972637223'),
+	   ('tranductien','20200781', '20200781','0987633321'),
+	   ('daovietbao','20200910','20200910','0978234765')
